ad1d4af2b1def86ca749d78a01fbdd79e82b7a12
navyata24 <navyatabhatt@gmail.com>
2026-08-03 22:49:44 +0530

    Refactor list operations and tuple printing
    Refactored input handling and output for greatest and smallest numbers. Improved list element swapping and updated tuple iteration.

--- Practical/Session2.py
+++ Practical/Session2.py
@@ -1,24 +1,31 @@
 # 1.
-elements = int(input("Enter the number of elements: "))
-list_of_elements = []
-for i in range(elements):
-    num = int(input("Enter element {}: ".format(i + 1)))
-    list_of_elements.append(num)
-print("List of elements:", list_of_elements)
-print("the greatest number is:", max(list_of_elements), "the smallest number is:", min(list_of_elements))
-list_of_elements.sort()
-print("the min and max is: ", list_of_elements[0], "and", list_of_elements[-1])
+n = int(input("Enter the number of elements: "))
+numbers = []
+
+for i in range(n):
+    value = int(input(f"Enter element {i + 1}: "))
+    numbers.append(value)
+
+print("List of elements:", numbers)
+print("Greatest number:", max(numbers))
+print("Smallest number:", min(numbers))
+
+numbers.sort()
+print("Minimum and Maximum are:", numbers[0], "and", numbers[-1])
+
 
 # 2.
-list1 = [1, 2, 3]
-list1[0], list1[-1] = list1[-1], list1[0]  # Swapping first and last elements
-#a = list1[0]
-#b = list1[-1]
-#list1[0] = b
-#list1[-1] = a
-print("List after swapping first and last elements:", list1)
+my_list = [1, 2, 3]
+
+first = my_list[0]
+my_list[0] = my_list[-1]
+my_list[-1] = first
+
+print("List after swapping first and last elements:", my_list)
+
 
 # 3.
-tupple1 = ("math", "java", "python", "physics", "chemistry")
-for i in tupple1:
-    print(i, end="\n")
+subjects = ("math", "java", "python", "physics", "chemistry")
+
+for subject in subjects:
+    print(subject)
